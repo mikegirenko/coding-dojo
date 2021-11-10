aa1930526c5ca391c38985977409be28671a04b0
mike.girenko <mike.girenko@cybergrx.com>
2021-11-10 11:42:48 -0700

    Learning what needs to be done

---
+++ args/code/args.py
@@ -0,0 +1,10 @@
+"""
+Actors:
+1. program to parse arguments
+2. parser
+        schema specifying what arguments accepted
+2. dictionary of arguments:
+        flags and with values
+        flag is one character preceded by minus sign (-)
+        value can be zero or one value
+"""
