b2c23658efe0a560adc8993bff334e33aff5510e
mike.girenko <mike.girenko@cybergrx.com>
2021-11-12 11:57:12 -0700

    Added another test

--- args/tests/test_program.py
+++ args/tests/test_program.py
@@ -1,9 +1,10 @@
-import pytest
-
 from args.code.program import MyProgram
 
 
-def test_it():
+def test_program_accepts_list_of_args():
     list_1 = [1,2]
     obj = MyProgram(list_1)
     assert obj.accept_list_of_args() == list_1
+
+def test_program_verifies_schema():
+    pass
