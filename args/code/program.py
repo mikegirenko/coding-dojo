dbb30f5f0a875ee9c7abb34cdbb52da3ef2cbf13
mike.girenko <mike.girenko@cybergrx.com>
2021-12-03 11:39:23 -0700

    Updated Program so it passes arguments to Parser

--- args/code/program.py
+++ args/code/program.py
@@ -2,16 +2,25 @@ from args.code.parser import MyParser
 
 class MyProgram:
     def __init__(self, list_of_args):
-        self.this_list = {}
-        self.this_list = list_of_args
+        self.accepted_list = {}
+        self.accepted_list = list_of_args
 
     def accept_list_of_args(self):
-        return self.this_list
+        return self.accepted_list
 
-    def pass_arguments_to_parser(self):
-        MyParser.verify_arguments()
+    def make_schema(self):
+        schema = []
+        for key in self.accepted_list.keys():
+            schema.append(key)
+        return schema
+
+    def pass_arguments_to_parser(self): # TODO continue here
+        schema = self.make_schema()
+        obj = MyParser(schema, self.accepted_list)
+        obj.verify_arguments()
+
 
     def return_one_flag_value(self, one_argument_flag):
-        for flag in self.this_list.keys():
+        for flag in self.accepted_list.keys():
             if flag == one_argument_flag:
-                return self.this_list[flag]
+                return self.accepted_list[flag]
--- args/tests/test_program.py
+++ args/tests/test_program.py
@@ -9,12 +9,19 @@ def test_program_accepts_list_of_args():
     assert obj.accept_list_of_args() == arguments_list
 
 
-def test_program_passes_arguments_to_parser():
+def test_program_passes_arguments_to_parser(): # TODO continue here
     schema = ["-l", "-p", "-d"]
     arguments_list = {"-l": False, "-p": 8080, "-d": "/usr/logs"}
     obj = MyProgram(arguments_list)
+    obj.pass_arguments_to_parser()
 
-    assert obj.pass_arguments_to_parser()
+
+
+def test_make_schema():
+    arguments_list = {"-l": False, "-p": 8080, "-d": "/usr/logs"}
+    expected_schema = ['-l', '-p', '-d']
+    obj = MyProgram(arguments_list)
+    assert obj.make_schema() == expected_schema
 
 
 def test_program_asks_for_value_of_specific_flag():
