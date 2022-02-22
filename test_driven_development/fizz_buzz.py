7bd8a4539621320d48ae29fd592cba12a066b891
mike.girenko <mike.girenko@cybergrx.com>
2022-02-22 15:56:21 -0700

    Added more tests. Refactoring

--- test_driven_development/fizz_buzz.py
+++ test_driven_development/fizz_buzz.py
@@ -1,12 +1,18 @@
 """
 Given a positive integer N
-Divisible by 3 → print “Fizz”
-Divisible by 5 → print “Buzz”
-Divisible by 3 and 5 → print “FizzBuzz”
-Otherwise → print N
+ Divisible by 3 → print “Fizz”
+ Divisible by 5 → print “Buzz”
+ Divisible by 3 and 5 → print “FizzBuzz”
+ Otherwise → print N
 """
 
 
 def fizz_buzz(positive_integer):
-    if positive_integer % 3 == 0:
+    if positive_integer % 3 == 0 and positive_integer % 5 != 0:
         return "Fizz"
+    if positive_integer % 5 == 0 and positive_integer % 3 != 0:
+        return "Buzz"
+    if positive_integer % 3 == 0 and positive_integer % 5 == 0:
+        return "FizzBuzz"
+    else:
+        return positive_integer
--- test_driven_development/fizz_buzz_unit_tests.py
+++ test_driven_development/fizz_buzz_unit_tests.py
@@ -6,3 +6,21 @@ import pytest
 def test_print_fizz_when_n_is_3(input_int):
     output = fizz_buzz(input_int)
     assert output == "Fizz"
+
+
+@pytest.mark.parametrize("input_int", [5, 10])
+def test_print_buzz_when_n_is_5(input_int):
+    output = fizz_buzz(input_int)
+    assert output == "Buzz"
+
+
+@pytest.mark.parametrize("input_int", [15, 30])
+def test_print_fizz_buzz_when_n_is_15(input_int):
+    output = fizz_buzz(input_int)
+    assert output == "FizzBuzz"
+
+
+@pytest.mark.parametrize("input_int,expected_out", ([1, 1], [2, 2]))
+def test_print_n(input_int,expected_out):
+    output = fizz_buzz(input_int)
+    assert output == expected_out
