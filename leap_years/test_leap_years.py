019a4b0322dfb353a5d7f5c61780a0a4257a1cf2
mike.girenko <mike.girenko@cybergrx.com>
2022-07-27 14:38:35 -0600

    Parametrizing tests

--- leap_years/test_leap_years.py
+++ leap_years/test_leap_years.py
@@ -1,11 +1,13 @@
+import pytest
 from leap_years.leap_years import determine_if_year_is_leap_year
 
 
-def test_year_is_not_leap_year():
-    year = 2017
+@pytest.mark.parametrize("year", [2017, 2018, 2019])
+def test_year_is_not_leap_year(year):
     assert not determine_if_year_is_leap_year(year)
 
 
-def test_year_is_leap_year():
+@pytest.mark.parametrize("year", [2008, 2012, 2016])
+def test_year_is_leap_year(year):
     year = 2000
     assert determine_if_year_is_leap_year(year)
