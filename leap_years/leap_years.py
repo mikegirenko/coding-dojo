6131cdb46597c9c56aa451f8c1774be82b46d1fa
mike.girenko <mike.girenko@cybergrx.com>
2022-07-28 11:48:39 -0600

    Completed Leap Years kata

--- leap_years/leap_years.py
+++ leap_years/leap_years.py
@@ -1,10 +1,28 @@
+from typing import List
 
-# All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016) DONE
 
-def determine_if_year_is_leap_year(year_to_check):
+def read_input() -> List[str]:
+    with open("years_list.txt", "r") as file:
+        read_data = file.read()
+    years = read_data.split("\n")
+
+    return years
+
+
+def determine_if_year_is_leap_year(year_to_check) -> bool:
     is_leap = False
     if year_to_check % 4 == 0:
-        if year_to_check % 100 != 0:
+        if year_to_check % 100 != 0 or year_to_check % 400 == 0:
             is_leap = True
 
     return is_leap
+
+
+def which_year_is_leap() -> List[str]:
+    years = read_input()
+    leap_years = []
+    for year in years:
+        if determine_if_year_is_leap_year(int(year)):
+            leap_years.append(year)
+
+    return leap_years
--- leap_years/test_leap_years.py
+++ leap_years/test_leap_years.py
@@ -1,12 +1,23 @@
+import calendar
 import pytest
-from leap_years.leap_years import determine_if_year_is_leap_year
+from leap_years.leap_years import *
 
 
-@pytest.mark.parametrize("year", [2017, 2018, 2019])
+@pytest.mark.parametrize("year", [1700, 1800, 1900, 2017, 2018, 2019, 2100])
 def test_year_is_not_leap_year(year):
     assert not determine_if_year_is_leap_year(year)
 
 
-@pytest.mark.parametrize("year", [2008, 2012, 2016])
+@pytest.mark.parametrize("year", [2000, 2008, 2012, 2016])
 def test_year_is_leap_year(year):
     assert determine_if_year_is_leap_year(year)
+
+
+def test_read_file():
+    assert len(read_input()) == 130
+
+
+def test_which_year_is_leap():
+    leap_years = which_year_is_leap()
+    for year in leap_years:
+        assert calendar.isleap(int(year))
