a027ffcc0f4b7f323325302372ab6af22e96b12b
mike.girenko <mike.girenko@cybergrx.com>
2022-10-10 16:03:39 -0600

    Handling Pair wins scenario

--- poker_hands/poker_hands.py
+++ poker_hands/poker_hands.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Any
+from typing import List
 
 INPUT_FILE = "poker_hands_input.txt"
 
@@ -11,7 +11,7 @@ def read_input_data(a_file) -> List[str]:
     return list_of_data
 
 
-def prepare_hands_for_comparing(two_hands_of_cards) -> Tuple[Any, Any]:
+def prepare_hands_for_comparing(two_hands_of_cards):
     two_pairs_split = two_hands_of_cards.split("  ")
     first_pair = two_pairs_split[0]
     second_pair = two_pairs_split[1]
@@ -28,8 +28,6 @@ def prepare_hands_for_comparing(two_hands_of_cards) -> Tuple[Any, Any]:
 # Compare TWO pairs of poker hands to indicate which has higher rank
 def compare_two_hands(player_one, hand_one, player_two, hand_two) -> str:
     game_result = ""
-    winner = ""
-    winning_card = ""
     hand_one_values = []
     hand_two_values = []
     for card in hand_one:
@@ -38,16 +36,45 @@ def compare_two_hands(player_one, hand_one, player_two, hand_two) -> str:
         hand_two_values.append(card[:1])
     first_hand_just_cards_list = hand_one
     second_hand_just_cards_list = hand_two
-    for i in range(len(first_hand_just_cards_list)):
-        if first_hand_just_cards_list[i] != second_hand_just_cards_list[i]:
-            if hand_one_values[i] > hand_two_values[i]:
-                winner = player_one
-                winning_card = str(hand_one_values[i])
-            if hand_one_values[i] < hand_two_values[i]:
-                winner = player_two
-                winning_card = str(hand_one_values[i])
-            game_result = winner + " wins. - with high card: " + winning_card
+
+    if pair(hand_one_values) and pair(hand_two_values):
+        hand_one_pair = pair(hand_one_values)
+        hand_two_pair = pair(hand_two_values)
+        if hand_one_pair > hand_two_pair:
+            winner = player_one
+            winning_card = hand_one_pair[0]
+            game_result = winner + " wins. - with pair: " + winning_card
         else:
-            game_result = "Tie"
+            winner = player_two
+            winning_card = hand_two_pair[0]
+            game_result = winner + " wins. - with pair: " + winning_card
+
+    if not pair(hand_one_values) or not pair(hand_two_values):
+        for i in range(len(first_hand_just_cards_list)):
+            if first_hand_just_cards_list[i] != second_hand_just_cards_list[i]:  # Not a Tie
+                if hand_one_values[i] > hand_two_values[i]:  # High Card
+                    winner = player_one
+                    winning_card = str(hand_one_values[i])
+                    game_result = winner + " wins. - with high card: " + winning_card
+                if hand_one_values[i] < hand_two_values[i]:  # High Card
+                    winner = player_two
+                    winning_card = str(hand_one_values[i])
+                    game_result = winner + " wins. - with high card: " + winning_card
+            else:
+                game_result = "Tie"  # Tie
 
     return game_result
+
+
+def pair(hand_of_cards): # find if hand has pair
+    duplicates = []
+    seen = set()
+    for card in hand_of_cards:
+        if card in seen:
+            duplicates.append(card)
+        else:
+            seen.add(card)
+
+    return duplicates
+
+ # TODO continue with two Pairs logic
--- poker_hands/test_poker_hands.py
+++ poker_hands/test_poker_hands.py
@@ -1,5 +1,5 @@
 from poker_hands.poker_hands import read_input_data, INPUT_FILE, compare_two_hands, \
-    prepare_hands_for_comparing
+    prepare_hands_for_comparing, pair
 
 
 def test_input_reading():
@@ -23,9 +23,32 @@ def test_compare_hands_when_tie():
 
 
 def test_compare_hands_when_highest_card_wins():
-    two_hands = "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH"
+    two_hands = "Black: 2H 3D 5S 9C KD  White: 2C 3H 5S 8C KH"
     player_one, hand_one, _, _ = prepare_hands_for_comparing(two_hands)
     _, _, player_two, hand_two = prepare_hands_for_comparing(two_hands)
 
     assert compare_two_hands(player_one, hand_one, player_two, hand_two) == \
            "Black wins. - with high card: 9"
+
+
+def test_compare_hands_when_pair_wins():
+    two_hands = "Black: 2H 2D 5S 9C KD  White: 3C 3H 4S 8C KH"  # W wins by pair of 3
+    player_one, hand_one, _, _ = prepare_hands_for_comparing(two_hands)
+    _, _, player_two, hand_two = prepare_hands_for_comparing(two_hands)
+
+    assert compare_two_hands(player_one, hand_one, player_two, hand_two) == \
+           "White wins. - with pair: 3"
+
+
+def test_pair():
+    cards = [2, 2, 4, 5]
+    assert pair(cards) == [2]
+
+
+# Example
+"""
+Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH
+Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S
+Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH
+Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH
+"""
