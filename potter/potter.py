f2a2962b652a24605097f9feebc299b1fa55c896
mike.girenko <mike.girenko@cybergrx.com>
2022-11-04 10:52:32 -0600

    More progress through Potter kata

--- potter/potter.py
+++ potter/potter.py
@@ -1,8 +1,11 @@
 BOOKS = ["first book", "second book", "third book", "fourth book", "fifth book"]
-BASKET_ZERO = []  # basket is empty
-BASKET_ONE = [BOOKS[0]]  # one book
-BASKET_TWO = [BOOKS[0], BOOKS[0]]  # two identical books
-BASKET_THREE = [BOOKS[0], BOOKS[1]]  # two different books - 5% discount
+BASKET_EMPTY = []  # basket is empty
+BASKET_ONE_BOOK = [BOOKS[0]]  # one book
+BASKET_TWO_BOOKS = [BOOKS[0], BOOKS[0]]  # two identical books
+BASKET_TWO_DIFF_BOOKS = [BOOKS[0], BOOKS[1]]  # two different books - 5% discount
+BASKET_THREE_BOOKS = [BOOKS[0], BOOKS[1], BOOKS[2]]  # three different books - 10% discount
+BASKET_FOUR_BOOKS = [BOOKS[0], BOOKS[1], BOOKS[2], BOOKS[3]]  # four different books - 20% discount
+BASKET_FIVE_BOOKS = [BOOKS[0], BOOKS[1], BOOKS[2], BOOKS[3], BOOKS[4]]  # five different books - 25% discount
 
 
 def calculate_price(basket):
@@ -12,14 +15,47 @@ def calculate_price(basket):
         basket_cost = 0
     if len(basket) == 1:  # one book
         basket_cost = single_book_price * 1
-    if len(basket) == 2:
+    if len(basket) == 2:  # two books
         if len(set(basket)) == 1:
             basket_cost = single_book_price * 2
         if len(set(basket)) == 2:  # 5% discount
             discount = ((single_book_price * 2) * 5) / 100
             basket_cost = (single_book_price * 2) - discount
-
+    if len(basket) == 3:  # three books
+        if len(set(basket)) == 1:
+            basket_cost = single_book_price * 3
+        if len(set(basket)) == 2:  # 5% discount + one book cost
+            discount = ((single_book_price * 2) * 5) / 100
+            basket_cost = ((single_book_price * 2) - discount) + single_book_price
+        if len(set(basket)) == 3:  # 10% discount
+            discount = ((single_book_price * 3) * 10) / 100
+            basket_cost = (single_book_price * 3) - discount
+    if len(basket) == 4:  # four books
+        if len(set(basket)) == 1:
+            basket_cost = single_book_price * 4
+        if len(set(basket)) == 2:  # 5% discount + 5% discount
+            discount = ((single_book_price * 2) * 5) / 100
+            basket_cost = ((single_book_price * 2) - discount) + ((single_book_price * 2) - discount)
+        if len(set(basket)) == 3:  # 10% discount + one book cost
+            discount = ((single_book_price * 3) * 10) / 100
+            basket_cost = ((single_book_price * 3) - discount) + single_book_price
+        if len(set(basket)) == 4:  # 20% discount
+            discount = ((single_book_price * 4) * 20) / 100
+            basket_cost = ((single_book_price * 4) - discount)
+    if len(basket) == 5:  # five books
+        if len(set(basket)) == 1:
+            basket_cost = single_book_price * 5
+        if len(set(basket)) == 2:  # 5% discount + 10% discount
+            discount_one = ((single_book_price * 2) * 5) / 100
+            discount_two = ((single_book_price * 3) * 10) / 100
+            basket_cost = ((single_book_price * 2) - discount_one) + ((single_book_price * 3) - discount_two)
+        if len(set(basket)) == 3:  # 10% discount + one book cost + one book cost
+            discount = ((single_book_price * 3) * 10) / 100
+            basket_cost = ((single_book_price * 3) - discount) + single_book_price + single_book_price
+        if len(set(basket)) == 4:  # 5% discount + one book cost + one book cost + one book cost
+            discount = ((single_book_price * 2) * 5) / 100
+            basket_cost = ((single_book_price * 2) - discount) + single_book_price + single_book_price + single_book_price
+        if len(set(basket)) == 5:  # 25% discount
+            discount = ((single_book_price * 5) * 25) / 100
+            basket_cost = ((single_book_price * 5) - discount)
     return basket_cost
-
-
-# TODO next - If you buy 3 different books, you get a 10% discount.
--- potter/test_potter.py
+++ potter/test_potter.py
@@ -1,17 +1,30 @@
-from potter.potter import calculate_price, BASKET_ZERO, BASKET_ONE, BASKET_TWO, BASKET_THREE
+from potter.potter import *
 
 
 def test_calculate_basket_zero_price():
-    assert calculate_price(BASKET_ZERO) == 0
+    assert calculate_price(BASKET_EMPTY) == 0
 
 
 def test_calculate_basket_one_price():
-    assert calculate_price(BASKET_ONE) == 8
+    assert calculate_price(BASKET_ONE_BOOK) == 8
 
 
 def test_calculate_basket_two_price():
-    assert calculate_price(BASKET_TWO) == 16
+    assert calculate_price(BASKET_TWO_BOOKS) == 16
 
 
 def test_calculate_basket_three_price():
-    assert calculate_price(BASKET_THREE) == 15.2
+    assert calculate_price(BASKET_TWO_DIFF_BOOKS) == 15.2
+
+
+def test_calculate_basket_four_price():
+    assert calculate_price(BASKET_THREE_BOOKS) == 21.6
+
+
+def test_calculate_basket_five_price():
+    assert calculate_price(BASKET_FOUR_BOOKS) == 25.6
+
+
+def test_calculate_basket_six_price():
+    assert calculate_price(BASKET_FIVE_BOOKS) == 30.0
+
