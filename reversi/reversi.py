40f4b46e38d2b48ab69a0c1bacb0caffbfad4fc5
mike.girenko <mike.girenko@cybergrx.com>
2022-12-14 11:50:04 -0700

    Working on reversi kata

--- reversi/reversi.py
+++ reversi/reversi.py
@@ -1,24 +1,53 @@
-# print 8x8 board filled with .
-
-def print_board(cell):
-    print("\n              Game Started")
-    for i in range(8):
-        if cell_logic("B") == "B":
-            cell = "B"
-        populate_row(cell)
-
-
-def populate_cell(cell):
-    for i in range(8):
-        print(cell, end='     ')
-    print("\n")
+BOARD = [
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", "B", "W", ".", ".", "."],
+    [".", ".", ".", "W", "B", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."]
+]
 
 
-def populate_row(cell_value):
-    populate_cell(cell_value)
+def print_board(board):
+    print("\n            Game starts")
+    for row in board:
+        print(row)
 
 
-def cell_logic(cell_value):
-    if cell_value == "B":
-        cell = "B"
-    return cell
+def populate_board(board):
+    return board
+
+
+def check_if_can_be_flipped_row(row, my_disk):
+    index_to_flip = 0
+    for disk in row:
+        if disk != my_disk and disk != ".":
+            index_to_flip = row.index(disk)
+
+    return index_to_flip
+
+
+def check_if_can_be_flipped_column(rows, my_disk):
+    index_to_flip = 0
+    index_to_check = 0
+    row_to_check = 0
+    my_disk_index = 0
+    my_disk_row_index = 0
+    row_counter = 0
+    # find my_disk index in a row which contains it
+    for row in rows:
+        if my_disk in row:
+            my_disk_index = row.index(my_disk)
+            my_disk_row_index = row_counter
+        row_counter += 1
+    print("my_disk_index", my_disk_index)
+    print("my_disk_row_index", my_disk_row_index)
+    # while index_to_check < 8: # need to check an index, for each row
+    #     if
+    #         # B is [1][3]. so check [0][3]?
+    #         row_to_check += 1
+    #     index_to_check += 1
+
+    return index_to_flip
--- reversi/test_reversi.py
+++ reversi/test_reversi.py
@@ -1,6 +1,27 @@
-from reversi.reversi import print_board
+from reversi.reversi import *
 
 
 def test_print_board():
-    empty_cell = "."
-    print_board(empty_cell)
+    print_board(BOARD)
+
+
+def test_populate_board():
+    board = populate_board(BOARD)
+    assert board[3][3] == "B"
+
+
+def test_check_if_can_be_flipped_row():
+    row = [".", ".", ".", "B", "W", ".", ".", "."]
+    index_can_be_flipped = check_if_can_be_flipped_row(row, "B")
+    assert index_can_be_flipped == 4
+
+
+def test_check_if_can_be_flipped_column():
+    rows = [
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", "W", "B", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+]
+    index_can_be_flipped = check_if_can_be_flipped_column(rows, "B")
+    # assert index_can_be_flipped == 4
