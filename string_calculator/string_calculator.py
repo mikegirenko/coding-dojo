331efc2088f8930e91b3145e766f48f90dbb0c65
mike.girenko <mike.girenko@cybergrx.com>
2023-02-13 14:43:03 -0700

    Continuing with String Calculator kata - finished missing number in last position

--- string_calculator/string_calculator.py
+++ string_calculator/string_calculator.py
@@ -6,36 +6,45 @@ class StringCalculator:
         list_by_n = []
         list_by_comma = []
         sum_of_numbers = ""
-        if string_of_numbers == "":
+        message = ""
+        if string_of_numbers == "" and "\n" not in string_of_numbers:
             sum_of_numbers = "0"
-        else:
-            if "\n" in string_of_numbers:
-                list_separated_by_comma = string_of_numbers.split(",")
-                for item in list_separated_by_comma:
-                    if "\n" in item:
-                        if len(item.splitlines()) > 1: # TODO continue here
-                            sum_of_numbers = "Number expected but '\n' found at position 6."
-                        else:
-                            list_of_numbers_separated_by_n = item.split("\n")
+        if string_of_numbers != "" and string_of_numbers[-1] == " ":
+            message = "Number expected but EOF found."
+        if "\n" in string_of_numbers:
+            list_separated_by_comma = string_of_numbers.split(",")
+            for item in list_separated_by_comma:
+                if "\n" in item:
+                    if item[:2] == " \n":
+                        if len(item.splitlines()) > 1:
+                            message = "Number expected but '\n' found at position 6."
                     else:
-                        list_2_of_numbers_separated_by_comma.append(item)
-                for item in list_of_numbers_separated_by_n:
-                    list_by_n.append(int(item))
-                for item in list_2_of_numbers_separated_by_comma:
-                    list_by_comma.append(int(item))
+                        list_of_numbers_separated_by_n = item.split("\n")
+                else:
+                    list_2_of_numbers_separated_by_comma.append(item)
+            for item in list_of_numbers_separated_by_n:
+                list_by_n.append(int(item))
+            for item in list_2_of_numbers_separated_by_comma:
+                list_by_comma.append(int(item))
+            if message:
+                sum_of_numbers = message
+            else:
                 sum_of_numbers = sum(list_by_n) + sum(list_by_comma)
-            if "\n" not in string_of_numbers:
-                list_of_strings = string_of_numbers.split(",")
-                for item in list_of_strings:
-                    list_of_numbers.append(int(item))
+        if "\n" not in string_of_numbers and string_of_numbers != "" and string_of_numbers[-1] != " ":
+            list_of_strings = string_of_numbers.split(",")
+            for item in list_of_strings:
+                list_of_numbers.append(int(item))
 
-                sum_of_numbers = sum(list_of_numbers)
-
+            sum_of_numbers = sum(list_of_numbers)
+        if message:
+            sum_of_numbers = message
         return sum_of_numbers
 
 
 if __name__ == "__main__":
     obj = StringCalculator()
     print(obj.add(""))
-    print(obj.add("1, 2, 3"))
-    print(obj.add("1\n2, 3"))
+    print(obj.add("1,2,3"))
+    print(obj.add("1\n2,3"))
+    print(obj.add("175, \n35"))
+    print(obj.add("1,3, "))
--- string_calculator/test_string_calculator.py
+++ string_calculator/test_string_calculator.py
@@ -4,18 +4,20 @@ obj = StringCalculator()
 
 
 def test_many_numbers():
-    assert obj.add("1, 2, 3") == 6
+    assert obj.add("1,2,3") == 6
 
 
 def test_empty_string():
     assert obj.add("") == "0"
 
 
-# TODO this is failing now
 def test_new_line_character():
-    assert obj.add("1\n2, 3") == 6
+    assert obj.add("1\n2,3") == 6
 
 
-# TODO continue here
 def test_new_line_separator_invalid():
     assert obj.add("175, \n35") == "Number expected but '\n' found at position 6."
+
+
+def test_input_ends_in_separator():
+    assert obj.add("1,3, ") == "Number expected but EOF found."
