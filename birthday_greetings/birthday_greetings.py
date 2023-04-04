f59ff19e7e6214a21c8e4e628444b09c8515691b
mike.girenko <mike.girenko@cybergrx.com>
2023-04-04 13:46:26 -0600

    Mo workin through Birthday Greetings Kata

--- birthday_greetings/birthday_greetings.py
+++ birthday_greetings/birthday_greetings.py
@@ -2,14 +2,31 @@ INPUT_FILE = "list_of_friends"
 
 class SendBirthdayNote:
 
-    def read_input_file(self, file_name):
+    def read_input_file(self, file_name) -> list:
+        final_list_of_friends = []
         with open(file_name) as file_object:
-            lines = file_object.readlines()
+            lines = file_object.read()
+            list_of_friends = lines.split("\n")
+        for friend in list_of_friends[1:]: # removing  last_name, first_name, date_of_birth, email
+            clean_friend = friend.replace(" ", "")
+            final_list_of_friends.append(clean_friend)
 
-        return lines
+        return final_list_of_friends
 
-    def send_birthday_note(self):
-        pass
+    def determine_birthday_note(self, list_of_friends, current_date):
+        this_friend_gets_an_email = ""
+        for friend in list_of_friends:
+            split_friend = friend.split(",")
+            if split_friend[2] == current_date:
+                this_friend_gets_an_email = split_friend
+
+        return this_friend_gets_an_email
+
+    def generate_email(self, friend) -> tuple:
+        subject = "Happy birthday!"
+        body = f"Happy birthday, dear {friend[1]}!"
+
+        return subject, body
 
 
 if __name__ == "__main__":
--- birthday_greetings/test_birthday_greetings.py
+++ birthday_greetings/test_birthday_greetings.py
@@ -1,7 +1,18 @@
-from birthday_greetings.birthday_greetings import SendBirthdayNote
+from birthday_greetings.birthday_greetings import SendBirthdayNote, INPUT_FILE
 
 obj = SendBirthdayNote()
 
 
+def test_read_input_file():
+    print(obj.read_input_file(INPUT_FILE))
+
+
 def test_send_birthday_note():
-    obj.send_birthday_note()
+    list_of_friends = obj.read_input_file(INPUT_FILE)
+    print(obj.determine_birthday_note(list_of_friends, "1982/10/08"))
+    print(obj.determine_birthday_note(list_of_friends, "1975/09/11"))
+
+
+def test_generate_email():
+    friend = ['Doe', 'John', '1982/10/08', 'john.doe@foobar.com']
+    print(obj.generate_email(friend))
