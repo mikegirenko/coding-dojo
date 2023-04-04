d8e2de2762b6838c6c5b148343af0e4a8801da77
mike.girenko <mike.girenko@cybergrx.com>
2023-04-04 15:29:57 -0600

    Struggling through Birthday Greetings Kata, trying to handle "Friends born on February, 29th should have their Birthday greeted on February, 28th" scenario

--- birthday_greetings/birthday_greetings.py
+++ birthday_greetings/birthday_greetings.py
@@ -13,17 +13,31 @@ class SendBirthdayNote:
 
         return final_list_of_friends
 
-    def determine_birthday_note(self, list_of_friends, current_date):
+    def born_february_twenty_nine(self, friend):
+        born_february_twenty_nine = False
+        split_friend = friend.split(",")
+        date = split_friend[2]
+        if date[5:] == "02/29":
+            born_february_twenty_nine = True
+
+        return born_february_twenty_nine
+
+
+
+    def determine_birthday_friend(self, list_of_friends, current_date):
         this_friend_gets_an_email = ""
         for friend in list_of_friends:
             split_friend = friend.split(",")
+            if self.born_february_twenty_nine(str(split_friend)):
+                if current_date[5] == "02/28":
+                    print("yes")
             if split_friend[2] == current_date:
                 this_friend_gets_an_email = split_friend
 
         return this_friend_gets_an_email
 
     def generate_email(self, friend) -> tuple:
-        subject = "Happy birthday!"
+        subject = "Subject: Happy birthday!"
         body = f"Happy birthday, dear {friend[1]}!"
 
         return subject, body
@@ -31,3 +45,8 @@ class SendBirthdayNote:
 
 if __name__ == "__main__":
     obj = SendBirthdayNote()
+    list_of_friends = obj.read_input_file(INPUT_FILE)
+    current_date = "1982/10/08"
+    birthday_friend = obj.determine_birthday_friend(list_of_friends, current_date)
+    subject, body = obj.generate_email(birthday_friend)
+    print(subject, "\n", body)
--- birthday_greetings/test_birthday_greetings.py
+++ birthday_greetings/test_birthday_greetings.py
@@ -1,4 +1,4 @@
-from birthday_greetings.birthday_greetings import SendBirthdayNote, INPUT_FILE
+from birthday_greetings.birthday_greetings import *
 
 obj = SendBirthdayNote()
 
@@ -7,10 +7,22 @@ def test_read_input_file():
     print(obj.read_input_file(INPUT_FILE))
 
 
+def test_born_february_twenty_nine():
+    friend_born_february_twenty_nine = "Smith,Stephen,1985/02/29,mary.smith@foobar.com"
+    assert obj.born_february_twenty_nine(friend_born_february_twenty_nine)
+    friend_not_born_february_twenty_nine = "Ann, Mary, 1975/09/11, mary.ann@foobar.com"
+    assert not obj.born_february_twenty_nine(friend_not_born_february_twenty_nine)
+
+
 def test_send_birthday_note():
     list_of_friends = obj.read_input_file(INPUT_FILE)
-    print(obj.determine_birthday_note(list_of_friends, "1982/10/08"))
-    print(obj.determine_birthday_note(list_of_friends, "1975/09/11"))
+    print(obj.determine_birthday_friend(list_of_friends, "1982/10/08"))
+    print(obj.determine_birthday_friend(list_of_friends, "1975/09/11"))
+
+
+def test_send_birthday_note_february_twenty_nine(): # TODO continue here
+    list_of_friends = ["Smith,Stephen,1985/02/29,mary.smith@foobar.com"]
+    print(obj.determine_birthday_friend(list_of_friends, "1975/02/28"))
 
 
 def test_generate_email():
