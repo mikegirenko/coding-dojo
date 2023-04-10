951f90584cd620b6fb65257b117580153dd7ca92
mike.girenko <mike.girenko@cybergrx.com>
2023-04-10 13:46:55 -0600

    Updates

--- cupcake/cupcake.py
+++ cupcake/cupcake.py
@@ -1,10 +1,33 @@
+import random
+
+
 class Cupcake:
     def name(self) -> list:
-        item_name = ["Cake", "Cookie", "Cake with Chocolate", "Cookie with Chocolate",
-                    "Cookie with Chocolate and Peanuts", "Cookie with Peanuts and Chocolate"]
+        item_name = ["Cake", "Cookie", "Cake with Chocolate", "Cookie with Chocolate", "Cookie Peanuts",
+                     "Cookie with Chocolate and Peanuts", "Cookie with Peanuts and Chocolate"]
+        item_index = random.randint(0, 6)
 
-        return item_name
+        return item_name[item_index]
+
+    def price(self, item):
+        price = 0
+        if item == "Cake":
+            price = 1
+        if item == "Cookie":
+            price = 2
+        if item == "Cake with Chocolate":
+            price = 1.1
+        if item == "Cookie with Chocolate":
+            price = 2.1
+        if item == "Cookie Peanuts":
+            price = 2.2
+
+        return price
+
+
 
 if __name__ == "__main__":
     obj = Cupcake()
-    obj.name()
+    item = obj.name()
+    price = obj.price(item)
+    print("The cake is", item, " The price is", price)
--- cupcake/test_cupcake.py
+++ cupcake/test_cupcake.py
@@ -1,6 +1,13 @@
 from cupcake.cupcake import *
+
 obj = Cupcake()
 
 
 def test_cupcake():
-    assert obj.name()[0] == "Cake"
+    assert obj.name() in ["Cake", "Cookie", "Cake with Chocolate", "Cookie with Chocolate", "Cookie Peanuts",
+                          "Cookie with Chocolate and Peanuts", "Cookie with Peanuts and Chocolate"]
+
+
+def test_price():
+    item = "Cake"
+    assert obj.price(item) == 1
