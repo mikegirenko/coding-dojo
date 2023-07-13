173e1834a1a2568f8a1385a1a25ab0e180c09a0a
mike.girenko <mike.girenko@cybergrx.com>
2023-07-13 14:36:36 -0600

    Working on Christmas delivery kata - working on User Story 3

--- christmas_delivery/christmas_delivery.py
+++ christmas_delivery/christmas_delivery.py
@@ -13,13 +13,22 @@ class SantasSleight:
     def sleigh(self, toys_list:list, elf_list:list) -> list:
         loaded_sleight = []
         i = 0  # keep removing first elf
+        elf_went_on_delivery = []
         for toy in toys_list:
-            if not len(elf_list) == 0:  # need to handle 3 toys and 2 elfs
+            if len(toys_list) <= len(elf_list):
+                # elf_went_on_delivery.append(elf_list[i])
+                #elf_list.pop(i)
+                loaded_sleight.append(toy)
+            if len(toys_list) > len(elf_list):   # to handle 3 toys and 2 elfs
+                elf_went_on_delivery.append(elf_list[i])
                 elf_list.pop(i)
                 loaded_sleight.append(toy)
+                if len(elf_list) == 0:
+                    elf_list.append(elf_went_on_delivery[-1])
+                    loaded_sleight.append(toy)
 
         return loaded_sleight
-
+# TODO next User Story 3
 
 if __name__ == "__main__":
     obj = SantasSleight()
--- christmas_delivery/test_christmas_delivery.py
+++ christmas_delivery/test_christmas_delivery.py
@@ -2,7 +2,19 @@ from christmas_delivery.christmas_delivery import *
 
 obj = SantasSleight()
 
-def test_sleight():
+def test_sleight_two_elfs_two_toys():
     toys_list = ["Toy Machine", "Toy Machine"]
+    elf_list = ["Alabaster Snowball", "Bushy Evergreen"]
+    assert obj.sleigh(toys_list, elf_list) == toys_list
+
+
+def test_sleight_two_elfs_one_toy():
+    toys_list = ["Toy Machine"]
+    elf_list = ["Alabaster Snowball", "Bushy Evergreen"]
+    assert obj.sleigh(toys_list, elf_list) == toys_list
+
+
+def test_sleight_two_elfs_three_toys():
+    toys_list = ["Toy Machine", "Toy Machine", "Toy Machine"]
     elf_list = ["Alabaster Snowball", "Bushy Evergreen"]
     assert obj.sleigh(toys_list, elf_list) == toys_list
