02774e1beb3ab6b2abd69213816791af72c8d152
mike.girenko <mike.girenko@cybergrx.com>
2023-07-27 10:53:41 -0600

    Finished Christmas delivery kata

--- christmas_delivery/christmas_delivery.py
+++ christmas_delivery/christmas_delivery.py
@@ -2,34 +2,39 @@
 program to load Santa's Sleigh
 Currently only one Elf can put a present on his Sleigh at a time
 Santa wants to be able to be able to use multiple Elves
-
-Alabaster Snowball, Bushy Evergreen, Pepper Minstix, Shinny Upatree,
-Sugarplum Mary, and Wunorse Openslae
 """
 
-elf_list = ["Alabaster Snowball", "Bushy Evergreen"]
 toy_machine = ["Present 1", "Present 2", "Present 3"]
+elf_list = ["Alabaster Snowball", "Bushy Evergreen"]
 presents_by_family = {"Smith family": 1, "Kaie family": 2}
 
+
 class SantasSleight:
-    def sleigh(self, toys_list:list, elf_list:list, presents_by_family:dict) -> list:
+    def sleigh(self, toys_list: list, elf_list: list, presents_by_family: dict) -> list:
         loaded_sleight = []
+        family_with_many_presents = False
+        for k, v in presents_by_family.items():
+            if v > 1:
+                family_with_many_presents = True
         i = 0  # it keeps removing first elf
         elf_went_on_delivery = []
-        for toy in toys_list:
-            while len(toys_list) != len(loaded_sleight):
+        while len(toys_list) != len(loaded_sleight):
+            for toy in toys_list:
                 if len(toys_list) <= len(elf_list):
                     loaded_sleight.append(toy)
-                if len(toys_list) > len(elf_list):   # to handle more toys than elfs
+                if len(toys_list) > len(elf_list):  # to handle more toys than elfs
                     elf_went_on_delivery.append(elf_list[i])
-                    elf_list.pop(i)
+                    if not family_with_many_presents:
+                        elf_list.pop(i)
+                    if family_with_many_presents:
+                        elf_list = elf_list
                     loaded_sleight.append(toy)
                     if len(elf_list) == 0:
                         elf_list.append(elf_went_on_delivery[-1])
                         loaded_sleight.append(toy)
 
         return loaded_sleight
-# TODO next User Story 3 - deliver presents by family when possible.
+
 
 if __name__ == "__main__":
     obj = SantasSleight()
--- christmas_delivery/test_christmas_delivery.py
+++ christmas_delivery/test_christmas_delivery.py
@@ -2,32 +2,29 @@ from christmas_delivery.christmas_delivery import *
 
 obj = SantasSleight()
 
+
 def test_sleight_two_elfs_two_toys():
     toy_machine = ["Present 1", "Present 2"]
     elf_list = ["Alabaster Snowball", "Bushy Evergreen"]
     presents_by_family = {"Smith family": 1, "Kaie family": 1}
-    assert len(obj.sleigh(toy_machine, elf_list, presents_by_family)) == len(toy_machine)
+    loaded_sleight = obj.sleigh(toy_machine, elf_list, presents_by_family)
+    assert len(loaded_sleight) == len(toy_machine)
+    assert loaded_sleight == toy_machine
 
 
 def test_sleight_two_elfs_one_toy():
     toy_machine = ["Present 1"]
     elf_list = ["Alabaster Snowball", "Bushy Evergreen"]
-    presents_by_family = {"Smith family": 1, "Kaie family": 1}
-    assert len(obj.sleigh(toy_machine, elf_list, presents_by_family)) == len(toy_machine)
+    presents_by_family = {"Smith family": 1}
+    loaded_sleight = obj.sleigh(toy_machine, elf_list, presents_by_family)
+    assert len(loaded_sleight) == len(toy_machine)
+    assert loaded_sleight == toy_machine
 
 
 def test_sleight_two_elfs_three_toys():
     toy_machine = ["Present 1", "Present 2", "Present 3"]
     elf_list = ["Alabaster Snowball", "Bushy Evergreen"]
-    presents_by_family = {"Smith family": 1, "Kaie family": 1}
-    assert len(obj.sleigh(toy_machine, elf_list, presents_by_family)) == len(toy_machine)
-
-
-def test_delivery_by_family():
-    toy_machine = ["Present 1", "Present 2", "Present 3"]
-    elf_list = ["Alabaster Snowball", "Bushy Evergreen"]
-    presents_by_family = {"Smith family": 2, "Kaie family": 1}
-    assert len(obj.sleigh(toy_machine, elf_list, presents_by_family)) == len(toy_machine)
-
-# one elf delivers all presents per family
-# if Smith family has 2 presents, "Alabaster Snowball" will take then and deliver
+    presents_by_family = {"Smith family": 1, "Kaie family": 2}  # note two toys
+    loaded_sleight = obj.sleigh(toy_machine, elf_list, presents_by_family)
+    assert len(loaded_sleight) == len(toy_machine)
+    assert loaded_sleight == toy_machine
